28a08eecf49c768dcfd897f8265efd54accf04d0
eslam <eslamelbazedy@gmail.com>
2021-10-20 23:14:55 +0200

    init commit

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('posts',[\App\Http\Controllers\PostController::class, 'index']);
+Route::get('posts/create',[\App\Http\Controllers\PostController::class, 'create']);
+
 Route::get('posts/{post}',[\App\Http\Controllers\PostController::class, 'show']);
