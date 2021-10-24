e406b447025bcf58d01adda65e1046f6e8751e5e
eslam <eslamelbazedy@gmail.com>
2021-10-24 22:23:09 +0200

    init commit

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/',[\App\Http\Controllers\PostController::class, 'index'])->name('posts.index');
 Route::get('posts',[\App\Http\Controllers\PostController::class, 'index'])->name('posts.index');
 Route::get('posts/create',[\App\Http\Controllers\PostController::class, 'create'])->name('posts.create');
 Route::get('posts/{post}',[\App\Http\Controllers\PostController::class, 'show'])->name('posts.show');
