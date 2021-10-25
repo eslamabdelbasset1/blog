b6957cea6712ad94e8f2b6a4bab3249f35ff0899
eslam <eslamelbazedy@gmail.com>
2021-10-26 01:58:09 +0200

    init commit

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,12 +3,31 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\storePostRequest;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return "hello";
+        $posts = Post::all();
+
+        return $posts;
+    }
+    public function show($post)
+    {
+        $post = Post::findOrFail($post);
+        return $post;
+    }
+
+    public function store(StorePostRequest $request)
+    {
+        $post = post::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'user_id' => $request->post_creator,
+        ]);
+        return $post;
     }
 }
